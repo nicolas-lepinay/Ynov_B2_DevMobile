56e06fbb8d62b9862799c9e953350f60e65d52fa
Nicolas Lépinay <nicolas@nosoft.io>
2026-03-23 14:53:01 +0100

    Create API Key

--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:netflix_clone/utils/constant.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -12,9 +13,91 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: kBackgroundColor,
       appBar: AppBar(
         leading: Image.asset('assets/images/netflix_logo_2.png'),
         backgroundColor: kBackgroundColor,
+      ),
+      body: ListView(
+        children: [
+          Container(height: 500, color: Colors.red),
+          const SizedBox(height: 16),
+          Text(
+            'Tendance actuelle',
+            style: GoogleFonts.poppins(
+              color: Colors.white,
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 10),
+          SizedBox(
+            height: 160,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: 10,
+              itemBuilder: (BuildContext context, int index) {
+                return Container(
+                  margin: const EdgeInsets.only(right: 8),
+                  width: 110,
+                  color: Colors.yellow,
+                  child: Center(child: Text(index.toString())),
+                );
+              },
+            ),
+          ),
+
+          const SizedBox(height: 24),
+          Text(
+            'Actuellement au cinéma',
+            style: GoogleFonts.poppins(
+              color: Colors.white,
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 10),
+          SizedBox(
+            height: 320,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: 10,
+              itemBuilder: (BuildContext context, int index) {
+                return Container(
+                  margin: const EdgeInsets.only(right: 8),
+                  width: 220,
+                  color: Colors.blue,
+                  child: Center(child: Text(index.toString())),
+                );
+              },
+            ),
+          ),
+          const SizedBox(height: 24),
+          Text(
+            'Bientôt disponibles',
+            style: GoogleFonts.poppins(
+              color: Colors.white,
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 10),
+          SizedBox(
+            height: 160,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: 10,
+              itemBuilder: (BuildContext context, int index) {
+                return Container(
+                  margin: const EdgeInsets.only(right: 8),
+                  width: 110,
+                  color: Colors.green,
+                  child: Center(child: Text(index.toString())),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
